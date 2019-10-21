a8cf13d5b492a2c4da3bd8987dec4c4a87cd95c4
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-21 14:16:10 -0500

    setup

--- lendr/src/index.js
+++ lendr/src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const reducers = combineReducers({ registrationReducer, itemReducer });
 
